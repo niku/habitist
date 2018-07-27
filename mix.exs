3a4c2d995a740bb2c72962b1016ca278aace534b
niku <niku@niku.name>
2018-07-27 09:12:51 +0900

    Use httpoison as httpclient

--- mix.exs
+++ mix.exs
@@ -25,7 +25,8 @@ defmodule Habitist.MixProject do
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:credo, "~> 0.9.3", only: :dev, runtime: false},
       {:dialyxir, "~> 1.0.0-rc.3", only: :dev, runtime: false},
-      {:floki, "~> 0.20.3"}
+      {:floki, "~> 0.20.3"},
+      {:httpoison, "~> 1.2"}
     ]
   end
 end
